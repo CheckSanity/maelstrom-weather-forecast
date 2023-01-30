15620af5cdf026b97c8625f1025085931bfcbf78
Garik Bezruchko <garik.bezruchko@yandex.ru>
2023-01-30 22:13:28 +0300

    added font + some new variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,8 @@ import './../assets/styles/_global.scss'
 
 import React from 'react'
 import StyledComponentsRegistry from '@/lib/registry'
-import { Josefin_Sans, Roboto } from '@next/font/google'
 import { CookiesProvider } from 'react-cookie'
 import { StoreProvider } from '@/providers/store'
-
-const roboto = Roboto({
-  weight: ['300', '400', '500'],
-  style: ['normal'],
-  subsets: ['latin'],
-})
-
-const josefinSans = Josefin_Sans({
-  weight: ['300', '400', '500'],
-  style: ['normal'],
-  subsets: ['latin'],
-})
 
 export default function RootLayout({
   children,
